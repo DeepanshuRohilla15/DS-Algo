44a4c54373f5efdccfb60e48daaedaea4265ccde
Deepanshu Rohilla <20001001027deepanshu@dcrustm.org>
2022-03-12 07:58:46 +0530

    NGL

---
+++ Next Greater to Left.cpp
@@ -0,0 +1,35 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int arr[] = {1,3,2,4};
+    int size = sizeof(arr)/sizeof(arr[0]);
+
+    vector<int> v;
+    stack<int> s;
+    for(int i=0;i<size;i++)
+    {
+        if(s.size()==0)  v.push_back(-1);
+        else if(s.size()>0 && s.top()>arr[i])
+        {
+            v.push_back(s.top());
+        }
+        else if(s.size()>0 && s.top()<= arr[i])
+        {
+            while(s.size()>0 && s.top()<=arr[i])
+            {
+               s.pop();
+            }
+            if(s.size()==0)  v.push_back(-1);
+            else{
+                v.push_back(s.top());
+            }
+        }
+        s.push(arr[i]);
+    } 
+    for(int i=0;i<v.size();i++)
+    {
+        cout<<v[i]<<" ";
+    }  
+}
