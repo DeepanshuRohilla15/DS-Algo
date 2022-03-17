72186ab22815fdfa747d238a6eb0ae0922896d6b
Deepanshu Rohilla <20001001027deepanshu@dcrustm.org>
2022-03-17 12:43:46 +0530

    Stock

---
+++ Stock Span Prob.cpp
@@ -0,0 +1,41 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int arr[]={100,80,60,70,60,75,85};
+    int size = sizeof(arr)/sizeof(arr[0]);
+
+    vector<int> v;
+    stack<pair<int,int>> s;
+
+    for(int i=0;i<size;i++)
+    {
+        if(s.size()==0) v.push_back(-1);
+
+        else if(s.size()>0 && s.top().first>arr[i])
+        {
+            v.push_back(s.top().second);
+        }
+        else if(s.size()>0 && s.top().first<= arr[i])
+        {
+            while(s.size()>0 && s.top().first<= arr[i])
+            {
+                s.pop();
+            }
+            if(s.size()==0) v.push_back(-1);
+            else{
+                v.push_back(s.top().second);
+            }
+        }
+        s.push({arr[i],i});
+    }
+    for(int i=0;i<v.size();i++)
+    {
+        v[i]= i-v[i];
+    }
+      for(int i=0;i<v.size();i++)
+    {
+        cout<<v[i]<<" ";
+    }  
+}
