7fa2b3a65b59658b0f9086306629edb7601ded4e
Deepanshu Rohilla <20001001027deepanshu@dcrustm.org>
2022-04-13 23:11:10 +0530

    heap

--- K closest number.cpp
+++ K closest number.cpp
@@ -1,9 +1,15 @@
 #include<bits/stdc++.h>
 using namespace std;
-int Kclosest(int arr[], int x, int k)
+
+
+int main()
 {
-    int size = sizeof(arr[1])/sizeof(arr[0]);
-    priority_queue<pair<int,int>> maxh;
+    int arr[]= {5,6,7,8,9};
+    int size =5;
+    int x = 7;
+    int k =3;
+   
+     priority_queue<pair<int,int>> maxh;
     for(int i=0; i<size;i++)
     {
        maxh.push({abs(arr[i]-x),arr[i]});
@@ -17,13 +23,4 @@ int Kclosest(int arr[], int x, int k)
         cout<<maxh.top().second<<" ";
         maxh.pop();
     }
-}
-
-int main()
-{
-    int arr[]= {5,6,7,8,9};
-    int x = 7;
-    int k =3;
-    Kclosest(arr, x, k);
-
 }
